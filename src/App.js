543f5aaff0fbc15b36ec92fe4211ac950c8a8ce9
Damanpreet Singh <daman0797@gmail.com>
2022-03-16 22:59:02 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar bg="dark" variant="dark" collapseOnSelect expand="md">
+        <Container fluid>
+          <Navbar.Brand href="home">Movie Hub</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#movies">Movies</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <FormControl 
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="secondary" href="#" new>Search</Button>{' '}
+          </Form>
+        </Container>
+      </Navbar>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
